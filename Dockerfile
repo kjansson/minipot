14365cb410a7612e7f4427329291edcd119277a2
Kim Jansson <kumpe82@gmail.com>
2022-06-23 09:55:58 +0200

    cleanup

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-#FROM alpine:3.15
-FROM scratch
-ADD minipot /
-ADD id_rsa /
-CMD ["/minipot"]
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CGO_ENABLED=0 go build -ldflags="-s"
-upx -9 minipot
-
-
-docker build --no-cache -t minipot:latest .
