431e10fb11163bd33c3abfe754091a28181758c2
Kim Jansson <kumpe82@gmail.com>
2022-06-27 23:03:52 +0200

    revert back to old docker consts

--- docker.go
+++ docker.go
@@ -1,29 +1,7 @@
 package main
 
-const DOCKER_FILE_BASE = `
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-ENTRYPOINT /entrypoint.sh
-`
-
-const PCAP_DOCKER_FILE = `
-FROM alpine
-COPY entrypoint.sh /entrypoint.sh
-RUN apk update && apk add tcpdump && chmod +x /entrypoint.sh
-ENTRYPOINT /entrypoint.sh
-`
-const PCAP_ENTRYPOINT = `
-#!/bin/sh
-tcpdump -i any -s 65535 -w /session.pcap
-`
-const ENTRYPOINT = `
-#!/bin/bash
-if [[ \"$USR\" != \"root\" ]]
-then
-	useradd -m -p thisisfake $USR -s /bin/bash
-	su - $USR
-else
-	bash
-fi
-`
+const DOCKER_FILE_BASE = "COPY entrypoint.sh /entrypoint.sh\nRUN chmod +x /entrypoint.sh\nENTRYPOINT /entrypoint.sh\n"
+const PCAP_DOCKER_FILE = "FROM alpine\nCOPY entrypoint.sh /entrypoint.sh\nRUN apk update && apk add tcpdump && chmod +x /entrypoint.sh\nENTRYPOINT /entrypoint.sh\n"
+const PCAP_ENTRYPOINT = "#!/bin/sh\ntcpdump -i any -s 65535 -w /session.pcap\n"
+const ENTRYPOINT = "#!/bin/bash\nif [[ \"$USR\" != \"root\" ]]\nthen\nuseradd -m -p thisisfake $USR -s /bin/bash\nsu - $USR\nelse\nbash\nfi\n"
 const DOCKER_CLIENT_ENV_NAME = "minipot-client-env:latest"
