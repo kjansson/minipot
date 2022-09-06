89f6a410a2c52a269d9563ef146e0aa60a79ebdf
Kim Jansson <kumpe82@gmail.com>
2022-09-06 19:55:38 +0200

    added permitattempt as possible env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MINIPOT_ENVS=( "DEBUG" "-debug" "OUTPUTDIR" "-outputdir" "ID" "-id" "HOSTNAME" "-hostname" "NETWORKMODE" "-networkmode" "SESSIONTIMEOUT" "-sessiontimeout" "PCAP" "-pcap" "PRIVATEKEY" "-privatekey" "BINDADDRESS" "-bindaddress" )
+MINIPOT_ENVS=( "DEBUG" "-debug" "OUTPUTDIR" "-outputdir" "ID" "-id" "HOSTNAME" "-hostname" "NETWORKMODE" "-networkmode" "SESSIONTIMEOUT" "-sessiontimeout" "PCAP" "-pcap" "PRIVATEKEY" "-privatekey" "BINDADDRESS" "-bindaddress" "PERMITATTEMPT" "permitattempt" )
 
 i=0
 for a in ${MINIPOT_ENVS[@]}; do
